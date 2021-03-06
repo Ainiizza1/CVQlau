5ee0e10f8baa192fd9d07fa6efde40f797e84838
asd <iqbal.ramadhani55@gmail.com>
2021-03-06 16:51:08 +0700

    Update distribusi.php

--- gudang/distribusi.php
+++ gudang/distribusi.php
@@ -8,15 +8,20 @@
   include_once('_partials/atas.php');
   include_once('_partials/kiri.php');
 
-  $ambil = $conn->query("SELECT t_penjualan.id_penjualan,tgl_setor,SUM(t_detail_penjualan.qty_terjual) jumlah_setor FROM t_penjualan JOIN t_detail_penjualan ON t_penjualan.id_penjualan = t_detail_penjualan.id_penjualan GROUP BY t_detail_penjualan.id_penjualan");
+  $tanggal = '2019-11-27';
+  $ambil = $conn->query("SELECT t_penjualan.id_penjualan,tgl_setor,SUM(t_detail_penjualan.qty_terjual) jumlah_setor FROM t_penjualan JOIN t_detail_penjualan ON t_penjualan.id_penjualan = t_detail_penjualan.id_penjualan WHERE DATE(t_penjualan.tgl_setor) <= '$tanggal' GROUP BY DATE(t_penjualan.tgl_setor) ORDER BY t_penjualan.tgl_setor DESC LIMIT 6");
   if (isset($_POST['kirim'])) {
     $produk1 = explode('#', $_POST['produk']);
     $tanggal = $_POST['tanggal'];
     $last_date = new DateTime($tanggal);
     $last_date->modify('-6 day');
     $tanggal = $last_date->format('Y-m-d');
-    $ambil = $conn->query("SELECT t_penjualan.id_penjualan,tgl_setor,SUM(t_detail_penjualan.qty_terjual) jumlah_setor FROM t_penjualan JOIN t_detail_penjualan ON t_penjualan.id_penjualan = t_detail_penjualan.id_penjualan WHERE DATE(t_penjualan.tgl_setor) <= '$tanggal' AND t_detail_penjualan.id_produk = $produk1[0] GROUP BY t_detail_penjualan.id_penjualan");
+    $ambil = $conn->query("SELECT t_penjualan.id_penjualan,tgl_setor,SUM(t_detail_penjualan.qty_terjual) jumlah_setor FROM t_penjualan JOIN t_detail_penjualan ON t_penjualan.id_penjualan = t_detail_penjualan.id_penjualan WHERE DATE(t_penjualan.tgl_setor) <= '$tanggal' AND t_detail_penjualan.id_produk = $produk1[0] GROUP BY t_detail_penjualan.id_penjualan ORDER BY t_penjualan.tgl_setor DESC LIMIT 6");
   }
+  $baru = array();
+  while ($pecah = $ambil->fetch_assoc()) $baru[] = $pecah;
+  $urutin = array_column($baru, 'tgl_setor');
+  array_multisort($urutin, SORT_ASC, $baru);
   ?>
 
   <div class="content-wrapper">
@@ -29,7 +34,7 @@
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="<?= $url; ?>gudang">Home</a></li>
               <li class="breadcrumb-item active">Kebutuhan Distribusi</li>
             </ol>
           </div><!-- /.col -->
@@ -80,7 +85,7 @@
                 <?php if (isset($produk1[1])) { ?>
                   <h6 class="mb-3">Jenis Roti : <b><?= $produk1[1]; ?></b></h6>
                 <?php } ?>
-                <table id="example1" class="table table-bordered table-hover">
+                <table class="example1 table table-bordered table-hover">
                   <thead>
                     <tr>
                       <th width="20px">No</th>
@@ -92,22 +97,28 @@
                     <?php
                     $nomor = 1;
                     $data_masukkan = array();
-                    while ($pecah = $ambil->fetch_assoc()) {
+                    for ($i = 0; $i < 6; $i++) {
+                      $tgl_setor = date('d-m-Y', strtotime($tanggal . ' -' . abs($i - 5) . ' day'));
+                      $status = false;
+                      foreach ($baru as $pecah) {
+                        if (date('d-m-Y', strtotime($pecah['tgl_setor'])) == $tgl_setor) {
+                          $status = true;
+                          break;
+                        }
+                      }
                       $data_masukkan[] = array(
-                        'tanggal' => $pecah['tgl_setor'],
-                        'setoran' => $pecah['jumlah_setor']
+                        'tanggal' => $tgl_setor,
+                        'setoran' => $status ? $pecah['jumlah_setor'] : 0
                       );
                     ?>
                       <tr>
-                        <td><?php echo $nomor; ?></td>
-                        <td><?php echo date('d-m-Y', strtotime($pecah['tgl_setor'])); ?></td>
-                        <td><?php echo $pecah['jumlah_setor']; ?></td>
+                        <td><?php echo $i + 1; ?></td>
+                        <td><?php echo  $tgl_setor; ?></td>
+                        <td><?php echo $status ? $pecah['jumlah_setor'] : 0; ?></td>
                       </tr>
                     <?php
-                      $nomor++;
                     }
                     ?>
-
                   </tbody>
                 </table>
               </div>
@@ -128,7 +139,7 @@
                 if ($ambil->num_rows > 2) {
                 ?>
                   <h3>Data Penjualan</h3>
-                  <table class="table table-bordered table-hover">
+                  <table class="example1 table table-bordered table-hover">
                     <thead>
                       <tr>
                         <td>No</td>
@@ -353,13 +364,149 @@
             <?php
             }
             ?>
-
           </section>
         </div>
+
+        <h4 class="mt-4">Analisis Distribusi Produk Kepada Sales</h4>
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">
+              <i class="fas fa-chart-pie mr-1"></i>
+              Halaman Kebutuhan Distribusi Produk Kepada Sales
+            </h3>
+          </div><!-- /.card-header -->
+          <div class="card-body">
+            <h3>Data Penjualan</h3>
+
+            <?php if (isset($produk1[1])) { ?>
+              <h6 class="mb-3">Jenis Roti : <b><?= $produk1[1]; ?></b></h6>
+            <?php } ?>
+            <table id="example1" class="table table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th rowspan="2" width="20px">No</th>
+                  <th rowspan="2">Sales</th>
+                  <th colspan="6">Tanggal</th>
+                  <th rowspan="2">Total</th>
+                  <th rowspan="2">Total Penjualan</th>
+                </tr>
+                <tr>
+                  <?php
+                  // $tanggal = $next_date->format('d-m-Y');
+                  for ($i = 6; $i > 0; $i--) {
+                  ?>
+                    <th><?php echo date('d-m-Y', strtotime($tanggal . ' -' . abs($i - 1) . ' day')); ?></th>
+                  <?php
+                  }
+                  ?>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                $no = 0;
+                $total_semua = 0;
+                $total_hari = array_fill(0, 6, 0);
+                $kumpul_datanya = array();
+                $ambil = $conn->query("SELECT t_users.id,t_sales.nama_sales,plat FROM t_sales JOIN t_users ON t_users.id = t_sales.id_user JOIN t_kendaraan ON t_kendaraan.id_kendaraan = t_sales.id_kendaraan");
+                while ($pecah = $ambil->fetch_assoc()) {
+                  $kumpul_datanya[]['sales'] = $pecah;
+                  $detail = $conn->query("SELECT t_penjualan.id_penjualan,DATE(tgl_setor) tgl_setor,SUM(t_detail_penjualan.qty_terjual) jumlah_setor FROM t_penjualan JOIN t_detail_penjualan ON t_penjualan.id_penjualan = t_detail_penjualan.id_penjualan WHERE DATE(t_penjualan.tgl_setor) <= '$tanggal' AND t_penjualan.id_sales = '" . $pecah['id'] . "' GROUP BY t_detail_penjualan.id_penjualan ORDER BY t_penjualan.tgl_setor DESC LIMIT 6");
+                  $data = array();
+                  while ($isi_detail = $detail->fetch_assoc()) $data[] = $isi_detail;
+                  $total = 0;
+                  for ($i = 6; $i > 0; $i--) {
+                    $status = false;
+                    foreach ($data as $datas) {
+                      if ($datas['tgl_setor'] == date('Y-m-d', strtotime($tanggal . ' -' . $i . ' day'))) {
+                        $status = true;
+                        $total += $datas['jumlah_setor'];
+                        $total_semua += $datas['jumlah_setor'];
+                        break;
+                      }
+                    }
+                    if ($status) {
+                      $kumpul_datanya[$no]['detail'][] = $datas['jumlah_setor'];
+                      $total_hari[abs($i - 6)] += $datas['jumlah_setor'];
+                    } else {
+                      $kumpul_datanya[$no]['detail'][] = 0;
+                      $total_hari[abs($i - 6)] += 0;
+                    }
+                    $kumpul_datanya[$no]['total'] = $total;
+                  }
+                  $no++;
+                }
+                $no = 1;
+                $total_persen = array();
+                foreach ($kumpul_datanya as $kd) :
+                ?>
+                  <tr>
+                    <td><?php echo $no; ?></td>
+                    <td><?php echo $kd['sales']['nama_sales']; ?></td>
+                    <?php
+                    for ($i = 0; $i < 6; $i++) {
+                    ?>
+                      <td><?php echo $kd['detail'][$i]; ?></td>
+                    <?php
+                    }
+                    ?>
+                    <td><?php echo $kd['total']; ?></td>
+                    <td><?php echo $kd['total'] > 0 ? $total_persen[] =  round(($kd['total'] / $total_semua) * 100, 2) : $total_persen[] = 0; ?>%</td>
+                  </tr>
+                <?php
+                  $no++;
+                endforeach;
+                ?>
+                <tr>
+                  <th colspan="2">Penjualan Produk/Hari</th>
+                  <?php for ($i = 0; $i < 6; $i++) { ?>
+                    <th><?php echo $total_hari[$i]; ?></th>
+                  <?php } ?>
+                  <th><?php echo $total_semua; ?></th>
+                </tr>
+              </tbody>
+            </table>
+
+            <h4 class="mt-5">Pembagian Jumlah Roti Setiap Sales</h4>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Sales</th>
+                  <th>Pembagian Roti Hari Selanjutnya</th>
+                  <th>Tanggal</th>
+                  <th>Plat Nomor</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                $no = 0;
+                foreach ($kumpul_datanya as $kd) {
+                ?>
+                  <tr>
+                    <td><?php echo $kd['sales']['nama_sales']; ?></td>
+                    <td><?php echo round($total_semua * ($total_persen[$no] / 100), 0); ?></td>
+                    <td><?php echo date('d-m-Y', strtotime($tanggal . ' +1 day')); ?></td>
+                    <td><?php echo $kd['sales']['plat']; ?></td>
+                  </tr>
+                <?php
+                  $no++;
+                }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+
+
         <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
+
+  <script>
+    let data = document.querySelector('#total0');
+    console.log(data);
+  </script>
 
   <?php include_once('_partials/bawah.php'); ?>
